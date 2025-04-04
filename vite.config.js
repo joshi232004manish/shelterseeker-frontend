deff6fc422b41ac0a536f65a25efff319dc615d3
joshi232004manish <manishjoshi232004@gmail.com>
2025-04-04 09:26:01 +0530

    deploy commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"https://shelterseeker-backend.vercel.app/",
+        target:"https://shelterseeker-backend.vercel.app",
         secure:false,
       },
     },
