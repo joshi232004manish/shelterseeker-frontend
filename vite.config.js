d987f404439b3feb5d7ccab47c241784be7d3fe8
joshi232004manish <147503846+joshi232004manish@users.noreply.github.com>
2025-04-04 09:45:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy:{
       '/api':{
         target:"https://shelterseeker-backend.vercel.app",
-        secure:false,
+        secure:true,
       },
     },
   },
