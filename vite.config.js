6313e614d95526bbdc8dce48be3e5a388af9723b
joshi232004manish <manishjoshi232004@gmail.com>
2025-04-04 07:00:02 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"http://localhost:3000",
+        target:"https://shelterseeker-backend.vercel.app/",
         secure:false,
       },
     },
